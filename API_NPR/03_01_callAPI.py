07caae00ad9a9334068cfb833e54e0540f575bd2
palutz <stefano.paluello@gmail.com>
2013-01-20 13:57:24 +0100

    ...

---
+++ API_NPR/03_01_callAPI.py
@@ -0,0 +1,16 @@
+from urllib2 import urlopen
+from json import load
+
+url = "http://api.npr.org/query?apiKey="
+key = "API_KEY" # fake id only for the test
+url = url + key
+
+url = url + "&numResults=3&format=json&id="
+npr_id = raw_input("Which NPR ID do you want to query?")
+url += npr_id
+
+response = urlopen(url)
+json_obj = load(response)
+
+for story in json_obj['list']['story']:
+	print story['title']['$text']
